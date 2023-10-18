628a8679207f3238cd39c2a23664a51442857fdc
AhmedAyachi <aayachi032@gmail.com>
2023-10-18 18:45:16 +0100

    fix: resolvers returning null as data

--- src/GraphQLField/GraphQLField.js
+++ src/GraphQLField/GraphQLField.js
@@ -4,28 +4,30 @@ import {getArrayAsObject} from "../index.js";
 export default function GraphQLField(config){
     const mainResolver=config.resolve,resolvers={};
     let {args}=config;
-    if(typeof(args)==="function"){args=args()};
-    if(Array.isArray(args)){args=getArrayAsObject(args)};
-    config.args=args;
-    for(const key in args){
-        const argument=args[key];
-        if(argument){
-            const {resolve}=argument;
-            if(typeof(resolve)==="function"){
-                resolvers[key]=resolve;
-                delete argument.resolve;
-            };
+    if(args){
+        if(typeof(args)==="function"){args=args()};
+        if(Array.isArray(args)){args=getArrayAsObject(args)};
+        for(const key in args){
+            const argument=args[key];
+            if(argument){
+                const {resolve}=argument;
+                if(typeof(resolve)==="function"){
+                    resolvers[key]=resolve;
+                    delete argument.resolve;
+                };
+            }
         }
+        config.args=args;
     }
-    config.resolve=async (parent,args,context,info)=>{
+    config.resolve=(args&&(typeof(mainResolver)==="function"))?(async (parent,args,context,info)=>{
         for(const key in args){
             const resolver=resolvers[key];
-            if(typeof(resolver)==="function"){
+            if(resolver){
                 const value=resolver(args[key],args,context,info);
                 args[key]=value instanceof Promise?await value:value;
             }
         }
-        return typeof(mainResolver)==="function"?mainResolver(parent,args,context,info):null;
-    };
+        return mainResolver(parent,args,context,info);
+    }):mainResolver;
     return config;
 }
